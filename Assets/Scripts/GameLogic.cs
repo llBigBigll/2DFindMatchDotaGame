889888812f5309d07f83199e85d78d6b3fe74b4e
Nikita <nikitaulyankinipad@gmail.com>
2020-09-09 14:01:25 +0300

    added basic gameplay

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class GameLogic : MonoBehaviour
@@ -8,6 +9,7 @@ public class GameLogic : MonoBehaviour
     public GameObject timer;
     
     public GameObject SearchPanel;
+    public GameObject TaskPanel;
     
     public TextAsset HeroListFile;
 
@@ -16,15 +18,22 @@ public class GameLogic : MonoBehaviour
     private Text timerText;
 
     private List<string> searchingFieldList;
+    private List<string> taskList;
+    private List<string> foundList;
+    private List<string> listOfNames;
 
     // Start is called before the first frame update
     void Start()
     {
         timerText = timer.GetComponentInChildren<Text>();
-        List<string> listOfNames = getTexturesNames(HeroListFile);
+        listOfNames = getTexturesNames(HeroListFile);
+        foundList = new List<string>();
 
-        searchingFieldList = GetRandomElementsFromList(listOfNames,3);
+        searchingFieldList = GetRandomElementsFromList(listOfNames, 8);
+        taskList = GetRandomElementsFromList(searchingFieldList, 3);
+
         InstantiateSearchingList();
+        InstantiateTaskList();
     }
 
     // Update is called once per frame
@@ -63,10 +72,28 @@ public class GameLogic : MonoBehaviour
             }
 
             var heroCell = Instantiate(HeroPlatePrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            
+            heroCell.name = heroName;
             heroCell.transform.SetParent(SearchPanel.transform, false);
             heroCell.transform.GetChild(1).GetComponent<Image>().sprite = sprite;
+            var eventTriger = heroCell.transform.GetChild(0).GetComponent<EventTrigger>();
+            AddEventTriggerListener(eventTriger, EventTriggerType.PointerClick, onPlateMouseDown);
 
+        }
+    }
+    private void InstantiateTaskList() 
+    {
+        foreach (var heroName in taskList)
+        {
+            var sprite = Resources.Load<Sprite>($"HeroesPics/{heroName}");
+            if (sprite == null) 
+            {
+                Debug.Log($"Не удалось найти текстуру с именем: \"{heroName}\"");
+            }
+
+            var heroCell = Instantiate(HeroPlatePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            heroCell.name = heroName;
+            heroCell.transform.SetParent(TaskPanel.transform, false);
+            heroCell.transform.GetChild(1).GetComponent<Image>().sprite = sprite;
         }
     }
     private static List<T> GetRandomElementsFromList<T>(List<T> list, int numberOfElements, bool canRepeat = false) 
@@ -96,6 +123,76 @@ public class GameLogic : MonoBehaviour
             }
         }
 
+        return answer;
+    }
+
+    public static void AddEventTriggerListener(EventTrigger trigger,
+                                           EventTriggerType eventType,
+                                           System.Action<BaseEventData> callback)
+    {
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = eventType;
+        entry.callback = new EventTrigger.TriggerEvent();
+        entry.callback.AddListener(new UnityEngine.Events.UnityAction<BaseEventData>(callback));
+        trigger.triggers.Add(entry);
+    }
+
+    public void onPlateMouseDown(BaseEventData data)
+    {
+        var other = (PointerEventData)data;
+        GameObject background = other.pointerEnter;
+        string name = other.pointerEnter.transform.parent.gameObject.name;
+        if (taskList.Contains(name) && !foundList.Contains(name)) 
+        {
+            foundList.Add(name);
+            var activeColor = Color.yellow;
+            activeColor.a = 0.6f;
+            background.GetComponent<Image>().color = activeColor;
+        }
+        if (!taskList.Contains(name)) 
+        {
+            var wrongColor = Color.red;
+            wrongColor.a = 0.6f;
+            background.GetComponent<Image>().color = wrongColor;
+        }
+
+        if (CheckWin()) 
+        {
+            ResetMg();
+        }
+    }
+
+    private void ResetMg() 
+    {
+        for (int i = 0; i < TaskPanel.transform.childCount; i++)
+        {
+            Destroy(TaskPanel.transform.GetChild(i).gameObject);
+        }
+
+        for (int i = 0; i < SearchPanel.transform.childCount; i++)
+        {
+            Destroy(SearchPanel.transform.GetChild(i).gameObject);
+        }
+
+        foundList = new List<string>();
+
+        searchingFieldList = GetRandomElementsFromList(listOfNames, 8);
+        taskList = GetRandomElementsFromList(searchingFieldList, 3);
+
+        InstantiateSearchingList();
+        InstantiateTaskList();
+    }
+    private bool CheckWin() 
+    {
+        bool answer = true;
+        foreach (var task in taskList)
+        {
+            if (!foundList.Contains(task)) 
+            {
+                answer = false;
+                break;
+            }
+        }
         return answer;
     }
 
